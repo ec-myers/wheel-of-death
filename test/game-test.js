24e6c78c7449a692ef685db5f093a38e8ceb00fc
“Elyse <elysemyers1@gmail.com>
2019-09-01 16:03:07 -0600

    Add testing for function and instance of Game class

--- test/game-test.js
+++ test/game-test.js
@@ -1,6 +1,7 @@
 const expect = chai.expect;
 
 import chai from "chai";
+import Game from "../src/Game.js";
 import Round from "../src/Round.js";
 import Player from "../src/Player.js";
 
@@ -9,5 +10,13 @@ describe('Game', function() {
 
   beforeEach(function() {
     game = new Game()
+  });
+
+  it('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', () => {
+    expect(game).to.be.an.instanceOf(Game);
   });
 })
