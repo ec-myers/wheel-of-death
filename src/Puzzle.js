e87de686eb6d96586d33e2835a00a3875bf848fc
Sara Karsh <sara@Saras-MacBook-Pro.local>
2019-08-31 20:40:16 -0600

    Begin constructors on Player, Puzzle, Turn, Wheel, and generate random wheel method

---
+++ src/Puzzle.js
@@ -0,0 +1,17 @@
+class Puzzle {
+    constructor(puzzleData) {
+        this.category = puzzleData[category];
+        this.numWords = puzzleData[number_of_words]
+        this.totalLetters = puzzleData[total_number_of_letters]
+        this.firstWord = puzzleData[first_word]
+        this.description = puzzleData[description]
+        this.correctAnswer = puzzleData[correct_answer]
+
+    }
+
+        getNewPuzzle() {
+
+        }
+}
+
+export default Puzzle
---
+++ src/Wheel.js
@@ -0,0 +1,19 @@
+class Wheel {
+    constructor(data) {
+        this.data = data;
+    }
+
+   makeNewWheel() {
+       let arr = []
+       const pushMe = () => {
+           let randomNum = Math.floor(Math.random() * 22);
+           arr.length < 6 ? (arr.push(obj.wheel[randomNum]) && pushMe()) : arr
+       }
+       pushMe()
+       return arr;
+   }
+
+
+}
+
+export default Wheel
