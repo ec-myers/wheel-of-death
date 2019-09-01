246c8807765d5519051dbb6fe944821711a24dbc
“Elyse <elysemyers1@gmail.com>
2019-09-01 13:20:03 -0600

    Adds testing functionality for Player class properties and default states

---
+++ test/player-test.js
@@ -0,0 +1,36 @@
+const expect = chai.expect;
+
+import chai from 'chai';
+import Player from '../src/Player.js';
+
+describe('Player', () => {
+  let player;
+
+  beforeEach(function() {
+    player = new Player(1, 'Lester')
+  });
+
+  it('should be a function', () => {
+    expect(Player).to.be.a('function');
+  });
+
+  it('should be an instance of player', () => {
+    expect(player).to.be.an.instanceOf(Player);
+  });
+
+  it('should have a name', () => {
+    expect(player.name).to.equal('Lester');
+  });
+
+  it('should have an id', () => {
+    expect(player.id).to.equal(1);
+  });
+  
+  it('should have a default current score of 0', () => {
+    expect(player.currentScore).to.equal(0);
+  });
+
+  it('should have a default grand total of 0', () => {
+    expect(player.grandTotal).to.equal(0);
+  });
+});
