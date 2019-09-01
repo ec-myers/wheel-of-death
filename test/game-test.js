0f6e0a6cf00d402967c2063af82c9d406351571d
“Elyse <elysemyers1@gmail.com>
2019-09-01 15:54:47 -0600

    Adds boilerplate for testing Game class

---
+++ test/game-test.js
@@ -0,0 +1,13 @@
+const expect = chai.expect;
+
+import chai from "chai";
+import Round from "../src/Round.js";
+import Player from "../src/Player.js";
+
+describe('Game', function() {
+  let game;
+
+  beforeEach(function() {
+    game = new Game()
+  });
+})
