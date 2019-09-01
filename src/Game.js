b204d2c2546df7c1cdbc66811fc9114f9738dfe0
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-09-01 15:35:50 -0600

    Add function to name inputs

---
+++ src/Game.js
@@ -0,0 +1,16 @@
+import Player from './Player.js';
+
+class Game {
+  constructor() {
+    this.game = game;
+    this.players = [];
+    this.roundCounter = 0;
+  }
+
+  getPlayerInfoFromDom() {
+    let playerOne = new Player(1, )
+  }
+
+}
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ import $ from 'jquery';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+$('#splash__start--button--js').on('click', function() {
+  let playerOne= $('#splash__player--input--one--js').val()
+  let playerTwo= $('#splash__player--input--two--js').val()
+  let playerThree= $('#splash__player--input--three--js').val()
+  $('#player__one--name--js').text(playerOne);
+  $('#player__one--score--js').text('$0');
+  $('#player__two--name--js').text(playerTwo);
+  $('#player__two--score--js').text('$0');
+  $('#player__three--name--js').text(playerThree);
+  $('#player__three--score--js').text('$0');
+});
+
