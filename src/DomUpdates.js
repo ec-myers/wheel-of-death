21138a292c5b2fe5e2e2347bb629d300d432d7e9
“Elyse <elysemyers1@gmail.com>
2019-09-02 21:38:59 -0600

    Adds functionality to display current puzzle on the DOM

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -1,12 +1,39 @@
-import Player from './Player.js';
-import Game from './Game.js';
-import Puzzle from './Puzzle.js';
+// import Player from './Player.js';
+// import Game from './Game.js';
+// import Puzzle from './Puzzle.js';
 
 import $ from 'jquery';
 
+export default {
 
-function appendPlayers(players) {
-  players.forEach(player => {
-    $(`.player-${player.id}-name`).text(player.name);
-  })
+  // appendPlayers(players) {
+  //   players.forEach(player => {
+  //     $(`.player-${player.id}-name`).text(player.name);
+  //   })
+  // }
+  showPuzzle(puzzle) {
+    console.log(puzzle.correctAnswer)
+    let puzzleAnswer = this.displayPuzzle(puzzle.correctAnswer);
+    console.log(puzzleAnswer)
+    $('#section__displayPuzzle--js').html(`${puzzleAnswer}`);
+    // $('#list__displayPuzzle--letter').hide();
+    $('#span__category--js').text(puzzle.category);
+    $('#span__hint--js').text(puzzle.description);
+  },
+
+  displayPuzzle(answer) {
+    let letterList = `<ul class="container__displayPuzzle">`;
+    answer.forEach(letter => {
+      if (letter === " ") {
+        letterList += `<li class="list__displayPuzzle"><span class="span__displayPuzzle--space" id="list__displayPuzzle--letter">&nbsp</span></p>`;
+      } else if (letter === "-" || letter === "'" || letter === "&") {
+        letterList += `<li class="list__displayPuzzle"><span class="span__displayPuzzle--symbol" id="list__displayPuzzle--symbol">${letter}</span></p>`;
+      } else {
+        letterList += `<li class="list__displayPuzzle"><span class="span__displayPuzzle--letter" id="list__displayPuzzle--${letter}">${letter}</span></p>`;
+      }
+    });
+    letterList += `</ul>`;
+    return letterList;
+  },
 }
+
