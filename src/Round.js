0549e1f93616cef994d03bc78b95dc9cd89b51f3
“Elyse <elysemyers1@gmail.com>
2019-09-02 19:27:57 -0600

    Adds methods to create bank of puzzles and assign the round a random puzzle

--- src/Round.js
+++ src/Round.js
@@ -4,10 +4,12 @@ import Puzzle from './Puzzle.js';
 import Wheel from './Wheel.js';
 
 class Round {
-  constructor(players, currentPlayer, puzzle, wheel) {
+  constructor(players, currentPlayer, puzzles, wheel) {
     this.players = players;
     this.currentPlayer = currentPlayer;
-    this.puzzle = puzzle;
+    this.allPuzzles = puzzles;
+    this.puzzleBank = [];
+    this.puzzle = this.getNewPuzzle();
     this.wheel = wheel;
   }
 
@@ -16,9 +18,19 @@ class Round {
   }
 
   getNewPuzzle() {
-    let randomIndex = Math.floor(Math.random() * game.puzzleBank.length)
-    return game.puzzleBank[randomIndex]
+    this.createPuzzleBank();
+    let randomIndex = Math.floor(Math.random() * this.puzzleBank.length)
+    return this.puzzleBank[randomIndex];
   }
+
+  createPuzzleBank() {
+    let oneWordAnswers = this.puzzles.one_word_answers.puzzle_bank;
+    let twoWordAnswers = this.puzzles.two_word_answers.puzzle_bank;
+    let threeWordAnswers = this.puzzles.three_word_answers.puzzle_bank;
+    let fourWordAnswers = this.puzzles.four_word_answers.puzzle_bank;
+    let allPuzzles = [...oneWordAnswers, ...twoWordAnswers, ...threeWordAnswers, ...fourWordAnswers];
+    return allPuzzles.forEach(puzzle => this.puzzleBank.push(puzzle));
+}
 
   getNewWheel() {
 
