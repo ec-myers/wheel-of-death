c8678bace8a36ae429e084a5723604802163bd87
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-09-03 21:07:03 -0600

    Adds testing functionality to Round class for createPuzzleBank method
    Co-Authored By: Elyse Myers @ec-myers

--- test/round-test.js
+++ test/round-test.js
@@ -4,11 +4,22 @@ import chai from 'chai';
 import Round from '../src/Round.js';
 import Turn from '../src/Turn.js';
 import Player from '../src/Player.js';
+import data from '../src/data.js';
 
 describe('Round', function () {
   let round;
 
   beforeEach(function () {
-    round = new Round()
+    let players = ['Royce', 'Cliff', 'Jasper'];
+    let currentPlayer = {};
+    let puzzles = data.puzzles;
+    let wheel = data.wheel;
+    round = new Round(players, currentPlayer, puzzles, wheel)
   });
+
+  it.only('should create a puzzle bank', () => {
+    round.createPuzzleBank();
+    expect(round.puzzleBank.length).to.eql(192)
+  });
+
 })
