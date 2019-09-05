a0b21823d71bba1e84128060c36eb0c218567852
Kate Malone <katerosemalone@gmail.com>
2019-09-04 19:31:07 -0600

    Add quit method and some css

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Game from './Game.js';
 import $ from 'jquery';
 import Round from './Round';
 import domUpdates from './DomUpdates';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// An example of how you tell webpack to use an image (also need to link to it in the index.html
 
 import './images/background.png'
 
@@ -20,6 +20,10 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 $(document).ready(function () {
   $(".body").css("background-image", "url('https://cdn.dribbble.com/users/948461/screenshots/3913689/dribbble_halloween_animation.gif')");
+});
+
+$('.header__btn--quit').click(event => {
+  location.reload();
 });
 
 $('#splash__start--button--js').on('click', function() {
