b862a4198ddf1e3952ebfe3dd60547491ca3b8a2
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-09-07 16:02:08 -0600

    Add function to hide letters on domUpdates plus css

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -30,27 +30,34 @@ export default {
     $('.container__displayPuzzle').show();
   },
 
+  highlightCurrentPlayerBorder(currentPlayer) {
+    console.log(currentPlayer)
+    // let currentPlayer = round.currentPlayer;
+    $(`${currentPlayer}`).addClass()
+  },
+
   showPuzzle(puzzle) {
     let puzzleAnswer = this.displayPuzzle(puzzle.correctAnswer);
     $('#section__displayPuzzle--js').html(`${puzzleAnswer}`);
-    // $('#list__displayPuzzle--letter').hide();
+    $('.span__displayPuzzle--letter').hide();
     $('#span__category--js').text(puzzle.category);
     $('#span__hint--js').text(puzzle.description);
   },
   
   displayPuzzle(answer) {
-    let letterList = `<ul class="container__displayPuzzle">`;
+    let letterList = `<div class="container__displayPuzzle">`;
     answer.forEach(letter => {
       if (letter === " ") {
-        letterList += `<li class="list__displayPuzzle"><span class="span__displayPuzzle--space" id="list__displayPuzzle--letter">&nbsp</span></p>`;
+        letterList += `<p class="list__displayPuzzle--space"><span class="span__displayPuzzle--space" id="list__displayPuzzle--space">&nbsp</span></p>`;
       } else if (letter === "-" || letter === "'" || letter === "&") {
-        letterList += `<li class="list__displayPuzzle"><span class="span__displayPuzzle--symbol" id="list__displayPuzzle--symbol">${letter}</span></p>`;
+        letterList += `<p class="list__displayPuzzle--symbol"><span class="span__displayPuzzle--symbol" id="list__displayPuzzle--symbol">${letter}</span></p>`;
       } else {
-        letterList += `<li class="list__displayPuzzle"><span class="span__displayPuzzle--letter" id="list__displayPuzzle--${letter}">${letter}</span></p>`;
+        letterList += `<p class="list__displayPuzzle--letter"><span class="span__displayPuzzle--letter" id="list__displayPuzzle--${letter}">${letter}</span></p>`;
       }
     });
-    letterList += `</ul>`;
+    letterList += `</div>`;
     return letterList;
   },
+
 }
 
