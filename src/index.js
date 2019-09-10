7f9fd29fcd011af47e59c72e05a80ee67cba2ad9
Sara Karsh <sara@Saras-MacBook-Pro.local>
2019-09-10 05:33:30 -0600

    Retool modal styling to increase readability

--- src/index.js
+++ src/index.js
@@ -109,27 +109,29 @@ $('#section__vowels--js').on('click', (e) => {
   game.currentRound.buyAVowel(guessedVowel);
 });
 
-$('.btn__instructions').on('click', () => {
-  e.preventDefault;
-  showInstructionsModal();
-})
+$('.btn__instructions').click(showInstructionsModal);
+
 
 function showInstructionsModal() {
   $(`<div class='help__modal'>
-      <div class='help__modal--content'>
-        <h5 class='help__modal--header'>How To Play</h5>
+      <header class='modal__header'>
+        <button class='modal__close'>X</button>
+         <h4 class='help__modal--header'>How To Play</h5>
+      </header>
+        <div class='help__modal--content'>
           <ul class='modal__ul'>
-            <li>Each game is comprised of 4 rounds</li>
-            <li>The player whose turn it is will spin the wheel</li>
-            <li>If the output is a number, the player has the opportunity to select a consonant</li>
-            <li>If the player picks correctly, the value from the wheel is added to their current score and they 
-              can spin again to select another consonant</li>
-            <li>If the player wishes to guess a vowel, they must purchase one for 100 points</li>
-            <li>If the wheel output is 'Bankrupt,' that the entirety of that player's score up to that point is lost</li>
-            <li>If the wheel output is 'Lose a Turn,' that player's turn is over</li>
-            <li>If a player wishes to try and solve the whole puzzle, they may input their guess</li>
-            <li>If this guess is correct, that player's score is increased by 1000 points and the round is over</li>
-            <li>If this guess is incorrect, the next player may spin</li>
+            <li>A game is 4 rounds</li>
+            <li>There will be a new puzzle and wheel each round</li>
+            <li>Spin the wheel when it is your turn</li>
+            <li>If the output is a number, you have the opportunity to select a consonant</li>
+            <li>If you're right, the value from the wheel is added to your current score and the Reaper stays away
+            for another turn</li>
+            <li>If you want to guess a vowel, you must sell a piece of your soul for 100 points</li>
+            <li>If the wheel output is 'Bankrupt,' you lose all the points you've earned</li>
+            <li>If the wheel output is 'Lose a Turn,' you get one step closer to meeting your fate at the Reaper's hand</li>
+            <li>Use the input box to solve the whole puzzle, if you dare</li>
+            <li>If correct, your score is increased by 1000 points and the round is over</li>
+            <li>If this guess is incorrect, you better watch out for the Reaper while the next player spins</li>
             <li>The round is over when the puzzle is solved</li>
             <li>The winner of the round gets to keep all points accrued, which will be moved to their bank</li>
             <li>The losers have their points for the round taken by the Grim Reaper</li>
