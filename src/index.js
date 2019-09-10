4f8fbcc54ccaa6fea3ee85f3af3d4f138b63a534
Sara Karsh <sara@Saras-MacBook-Pro.local>
2019-09-10 08:06:53 -0600

    Remove commented out functions

--- src/index.js
+++ src/index.js
@@ -111,22 +111,10 @@ $('#section__vowels--js').on('click', (e) => {
 
 $('.btn__instructions').click(showInstructionsModal);
 
-
-// $('.modal__close').on('click', (e) => {
-//   console.log("Click!!")
-//   e.target.closest('.help__modal').addClass('.hidden');
-// })
-
-// $('.help__modal').on('click',(e) => {
-//   console.log("click clack")
-// })
-
-
-
-// $('.modal__close').click(e) => {
-//   console.log("Click!!")
-//   e.target.closest('.help__modal').hide();
-// }
+$('.modal__close').click(e) => {
+  console.log("Click!!")
+  e.target.closest('.help__modal').hide();
+}
 
 
 function showInstructionsModal() {
