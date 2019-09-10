f87ad4760e03a614f1f19d85bcc43db06650e751
Sara Karsh <sara@Saras-MacBook-Pro.local>
2019-09-09 20:29:41 -0600

    Refactor instructions modal for new class name and add button

--- src/index.js
+++ src/index.js
@@ -109,3 +109,34 @@ $('#section__vowels--js').on('click', (e) => {
   game.currentRound.buyAVowel(guessedVowel);
 });
 
+$('.btn__instructions').on('click', () => {
+  e.preventDefault;
+  showInstructionsModal();
+})
+
+function showInstructionsModal() {
+  $(`<div class='help__modal'>
+      <div class='help__modal--content'>
+        <h5 class='help__modal--header'>How To Play</h5>
+          <ul class='modal__ul'>
+            <li>Each game is comprised of 4 rounds</li>
+            <li>The player whose turn it is will spin the wheel</li>
+            <li>If the output is a number, the player has the opportunity to select a consonant</li>
+            <li>If the player picks correctly, the value from the wheel is added to their current score and they 
+              can spin again to select another consonant</li>
+            <li>If the player wishes to guess a vowel, they must purchase one for 100 points</li>
+            <li>If the wheel output is 'Bankrupt,' that the entirety of that player's score up to that point is lost</li>
+            <li>If the wheel output is 'Lose a Turn,' that player's turn is over</li>
+            <li>If a player wishes to try and solve the whole puzzle, they may input their guess</li>
+            <li>If this guess is correct, that player's score is increased by 1000 points and the round is over</li>
+            <li>If this guess is incorrect, the next player may spin</li>
+            <li>The round is over when the puzzle is solved</li>
+            <li>The winner of the round gets to keep all points accrued, which will be moved to their bank</li>
+            <li>The losers have their points for the round taken by the Grim Reaper</li>
+            <li>The winner of the game is determined by who has the most money in the bank at the end of four rounds</li>
+            <li>The losers are sent to the Reaper, never to be seen again</li>
+          </ul>
+        </div>
+      </div>
+    `).insertBefore('.results_instructions')
+}
